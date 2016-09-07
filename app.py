bd7009ed72d050781d5e84626d618a10462a6710
Indranil Dutta <duttaindranil497@gmail.com>
2016-09-07 09:22:47 +0530

    Added different routes, changed port number

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def main():
+    return redirect('/MU3.14159', code = 302)
+    
+@app.route("/repo")
+def repo():
+    return redirect('https://github.com/PyPals')
+
+@app.route("/website-repo")
+def web_repo():
+    return redirect('https://github.com/PyPals/pypals-website')
+
+@app.route("/MU3.14159")
+def mu_py():
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    app.run(port = 3000)
+
