8ff43beece03f94056506a24b3ff7923992dcf23
avikantz <avikantsainidbz@gmail.com>
2016-09-07 11:52:17 +0530

    Added logo

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ def repo():
 def web_repo():
     return redirect('https://github.com/PyPals/pypals-website')
 
+@app.route("/LUGM")
+def lugm():
+    return redirect('http://lugm.xyz/')
+
 @app.route("/MU3.14159")
 def mu_py():
     return render_template('index.html')
